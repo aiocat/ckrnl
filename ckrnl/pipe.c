ebec3ffbe3c2bba5617d8c76da9497c5de16fb05
aiocat <aiocat@protonmail.com>
2022-06-28 15:42:10 +0300

    add pipe active function

--- ckrnl/pipe.c
+++ ckrnl/pipe.c
@@ -1,1 +1,14 @@
-// TODO
+#include <winbase.h>
+#include <stdint.h>
+#include <string.h>
+
+#define PIPE_NAME "krnlpipe"
+#define PIPE_PATH "\\\\.\\pipe\\krnlpipe"
+
+uint8_t PipeActive()
+{
+    if (WaitNamedPipe(PIPE_PATH, 0) == 0)
+        return 0;
+    else
+        return 1;
+}
--- test/main.c
+++ test/main.c
@@ -1,9 +1,13 @@
 #include "../ckrnl/inject.c"
+#include "../ckrnl/pipe.c"
 #include <stdio.h>
 
 int main(void)
 {
+    printf("Before Inject Pipe Active: %d\n", PipeActive());
+
     puts("Injecting...");
     InjectionStatus result = Inject();
+
     printf("Got: %d\n", result);
 }
